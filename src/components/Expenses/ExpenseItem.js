a02bbd28b74f79c854345428eb3ed7a2e55a361c
Muhammad Ahamd Raza <csmirza11@gmail.com>
2022-01-25 12:03:22 +0500

    class(2) 1/25/2022

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,25 @@
+import React, {useState} from 'react'
+
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+
+ const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated')
+    console.log(title)
+  }
+
   return (
     <div className='expense-item'>
-        <div>{props.date.toISOString()}</div>
+        <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'> ${props.amount} </div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
     </div>
   );
 }
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+
+function Expenses(props) {
+
+    return(
+        <>
+        <Card>
+        <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+       />
+       
+             <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+       ></ExpenseItem>
+         <ExpenseItem
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+       ></ExpenseItem>
+         <ExpenseItem
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+       ></ExpenseItem>
+       
+       </Card>
+       </>
+    )
+}
+
+export default Expenses;
